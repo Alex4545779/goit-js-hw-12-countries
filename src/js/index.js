9ec20078b9411097d3df6c196ae8625c707d61dc
Aleksangr Goy <sashagoy87@gmail.com>
2021-09-06 01:46:24 +0300

    test 2

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,16 @@
-/* debounce from lodash */
 import _ from 'lodash';
 
-/* pnotify */
 import { error } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 import { defaults } from '@pnotify/core';
 defaults.maxTextHeight = null;
 
-/* Template */
 import countryCardTpl from '../templates/country-card.hbs';
 import countriesList from '../templates/countries-list.hbs';
 
-/* Additional parts js */
 import API from '../js/fetchCountries';
 import getRefs from '../js/get-refs';
-
-// const debounce = require('lodash.debounce');
 
 const refs = getRefs();
 refs.searchInput.addEventListener('input', _.debounce(onSearch, 500));
@@ -42,12 +36,9 @@ function renderCountryCard(countries) {
   }
   if (countries.length === 1) {
     refs.cardContainer.innerHTML = countryCardTpl(...countries);
-    //console.log(...countries);
   }
   if (countries.length >= 2 && countries.length <= 10) {
     refs.cardContainer.innerHTML = countriesList(countries);
-
-    //console.log(refs.cardContainer);
     refs.cardContainer.addEventListener('click', onItemClick);
   }
 
@@ -69,15 +60,6 @@ function renderCountryCard(countries) {
     });
   }
 }
-
-// function onFetchError() {
-//   error({
-//     title: 'Too many matches found.',
-//     text: ' Please enter a more specific query!',
-//     styling: 'brighttheme',
-//     delay: 2000,
-//   });
-// }
 
 function onFetchError(messageError) {
   error({
